1da9ac987ee3b37ef6e9e0494fb050b53cbf0771
Ever Ivan Barahona Mendoza <ever.barahona.mendoza@ocean.ibm.com>
2023-08-24 15:29:31 -0600

    new changes for env var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+
+
 export const environment = {
   production: true,
-  URL_USERS: "https://kdhexm3qap.us-east-1.awsapprunner.com/users",
-  URL_PRODUCTS:"https://ceapuskw2b.us-east-1.awsapprunner.com/products" //process.env ()
+  URL_USERS: process.env.URL_USERS, //"https://kdhexm3qap.us-east-1.awsapprunner.com/users",
+  URL_PRODUCTS: process.env.URL_PRODUCTS //"https://ceapuskw2b.us-east-1.awsapprunner.com/products"
 };
